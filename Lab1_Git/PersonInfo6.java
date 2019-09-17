dfe9cf32d70130ce0eb3cac678dc320b47093780
Enotho <mfren022@uottawa.ca>
2019-09-17 17:58:45 -0400

    added new student info

---
+++ Lab1_Git/PersonInfo6.java
@@ -0,0 +1,27 @@
+public class PersonInfo5 {
+
+    public static String firstName = "Matthieu";
+    public static String lastName = "Frenette";
+    public static int studentNo = 8172416;
+    public static String description = "sandwich";
+    
+    public static void displayFr() {
+        String out = SIConstants.GREETING_FR;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
+        out += SIConstants.DESCRIPTION_FR + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+    
+    public static void displayEn() {
+        String out = SIConstants.GREETING_EN;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
+        out += SIConstants.DESCRIPTION_EN + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+}
